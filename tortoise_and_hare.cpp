f8dfc5d9c9f4e82c2538b72f22f2bb3401a593f3
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2022-09-25 12:14:31 +0100

    fixed algorithm

--- tortoise_and_hare.cpp
+++ tortoise_and_hare.cpp
@@ -1,69 +1,85 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <set> 
-#include <math.h>
-using namespace std;
+// Title: Tortoise and hare
+// Name: Christopher Robert Kitching
+// E-mail: christopher.kitching@manchester.ac.uk
+// Date created: 24/09/22
+// Last editied: 25/09/22
+// Description: Implementation of tortoise and hare algorithm to find the
+//              number of unique elements in a cycling array
 
-// update position function
-long long f(const long long& pos, const long long& S, const long long& P, 
-            const long long& Q){
-    return (pos*P + Q);
-}
+// includes
+#include <iostream>
+#include <math.h>
+
+// preprocessor directive for recurrance function
+#define f(X, P, Q) (X*P + Q)
+
+// create alias for long long
+typedef unsigned long long ulong;
 
 // hare and tortoise algorithm to find cycle length
-int hare_and_tortoise(const long long N, const long long& S, 
-                      const long long& P, const long long& Q){
+int hare_and_tortoise(const int N, const int& S, 
+                      const int& P, const int& Q, 
+                      const ulong& k){
     
+    // initial position
+    ulong x_0{S%k};
+    ulong count{1};
+
     // initalise hare and tortoise positions
-    long long tortoise{f(S, S, P, Q)}, hare{f(f(S, S, P, Q), S, P, Q)};
+    ulong tortoise = f(x_0, P, Q)%k;
+    ulong hare = f(f(x_0, P, Q)%k, P, Q)%k;
     
     // find repetition in sequence
     while(tortoise != hare){
-        tortoise = f(tortoise, S, P, Q);
-        hare = f(f(hare, S, P, Q), S, P, Q);
+
+        // if tortoisue reached end of array without finding loop
+        // all elements are unique so return length of array
+        if(count >= N) return count;
+        else count++;
+
+        // update tortoise and hare position
+        tortoise = f(tortoise, P, Q)%k;
+        hare = f(f(hare, P, Q)%k, P, Q)%k;
     }
     
     // find position of the first repetition
-    int mu{0};
-    tortoise = S;
+    ulong mu{0};
+    tortoise = x_0;
     while(tortoise != hare){
-        tortoise = f(tortoise, S, P, Q);
-        hare = f(hare, S, P, Q);
-        mu += 1;
+        tortoise = f(tortoise, P, Q)%k;
+        hare = f(hare, P, Q)%k;
+        mu++;
     }
     
     // Find the length of the shortest cycle starting from x_mu
     // The hare moves one step at a time while tortoise is still.
     // lam is incremented until lam is found.
-    int lam{1};
-    hare = f(tortoise, S, P, Q);
+    ulong lam{1};
+    hare = f(tortoise, P, Q)%k;
     while(tortoise != hare){
-        hare = f(hare, S, P, Q);
-        lam += 1;
+        hare = f(hare, P, Q)%k;
+        lam++;
     }
     
-    return lam;
+    return lam + mu;
 }
 
 int main() {  
     
     // declare variables
-    long long N, S, P, Q, k;
-    k = pow(2, 31);
+    int N, S, P, Q;
     
     // read in data
-    N = 3;
-    S = 1;
-    P = 1;
-    Q = 1;
+    N = 100000000;
+    S = 569099406;
+    P = 1607140150;
+    Q = 823906344;
+
+    // cast modulo to long long
+    ulong k = static_cast<ulong>(std::pow(2, 31));
     
     // solve
-    std::cout << hare_and_tortoise(N, S, P, Q);
+    std::cout << hare_and_tortoise(N, S, P, Q, k);
     
     return 0;
 }
