5e0e682a3a94b9d300f08cf1bf6fa25153ef6d9d
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2022-10-16 12:24:31 +0100

    added leetcode problem

---
+++ leetcode.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+bool self_dividing(const int& num)
+{
+    int copy = num;
+    while(copy > 0){
+        int digit = copy%10;
+        if(digit == 0) return false;
+        if(num%digit != 0) return false;
+        copy /= 10;
+    }
+    return true;
+}
+
+std::vector<int> selfDividingNumbers(const int& left,const int& right) {
+    std::vector<int> nums;
+    for(int i{left}; i < right + 1; i++){
+        if(self_dividing(i)){
+            std::cout << i << std::endl;
+            nums.push_back(i);
+        }
+    }
+    return nums;
+}
+
+int main()
+{
+    int left = 1; 
+    int right = 22;
+    std::vector<int> nums = selfDividingNumbers(left, right);
+
+
+    return 0;
+}
