64faf3b423cb784defb8adc284fc19310eefc93e
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2022-10-24 10:50:32 +0100

    problems

--- HackerRank/Hackerrank.py
+++ HackerRank/Hackerrank.py
@@ -6,76 +6,39 @@
 # Description: File to test Hackerrank solutions
 
 
-def get_median(count, d):
-    
-    counter = 0
-    median = 0
-    
-    # odd length arrays
-    if d%2 != 0:
-        
-        # loop through counter
-        for i in range(len(count)):
-            
-            # incremenet until we find the midpoint
-            counter += count[i]
-            if counter > d//2:
-                median = i
-                break
-    
-    # even length arrays
-    else:
-        
-        first = 0
-        second = 0
-        
-        # loop through counter
-        for i in range(len(count)):
-            
-            # increment until we find middle points
-            counter += count[i]
-            if counter >= d//2:
-                median = (2*i+1)/2
-                break
-                
-    return median
-            
-
-def activityNotifications(expenditure, d):
+def minimumBribes(q):
     # Write your code here
-    
-    count = [0] * (max(expenditure) + 1)
-    
-    trailing_days = expenditure[:d]
-    for spend in trailing_days:
-        count[spend] += 1
+
+    # check all people in queue
+    for i, person in enumerate(q):
+
+        # if any person is more than 2 positions ahead of their original
+        # position then its too chatoic
+        if person - (i+1) > 2:
+            print("Too chaotic")
+            return
         
-    warnings = 0
-    for i in range(d, len(expenditure)):
-        
-        new_value = expenditure[i]
-        old_value = expenditure[i-d]
-        
-        median = get_median(count, d)
-        if expenditure[i] >= 2*median:
-            warnings += 1
+        # if person not at initial position
+        if person != i + 1:
             
-        count[new_value] += 1
-        count[old_value] -= 1
-        
-    return warnings
+            # count the number of people with a number larger than them 
+            # ranging from one position in front of their original position
+            # to one person in front of their current position
+            for j in range(max(person - 2, 0), i):
+                if q[j] > person:
+                    bribes += 1
             
+    print(bribes)
+    return 
         
-    
     
 def main():
     """Main function
     """
 
-    d = 5
-    arr = [2, 3, 4, 2, 3, 6, 8, 4, 5]
-    w = activityNotifications(arr, d)
-    print(w)
+    q = [2, 1, 5, 3, 4]
+    print(minimumBribes(q))
+
 
 # run file
 if __name__ == "__main__":
