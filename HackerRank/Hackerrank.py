775ee91f3d1c33ba5e2b84ee1dbb739eec0b42f7
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2022-10-27 10:59:52 +0100

    hackerrank

--- HackerRank/Hackerrank.py
+++ HackerRank/Hackerrank.py
@@ -6,40 +6,41 @@
 # Description: File to test Hackerrank solutions
 
 
-def minimumBribes(q):
-    # Write your code here
-
-    # check all people in queue
-    for i, person in enumerate(q):
-
-        # if any person is more than 2 positions ahead of their original
-        # position then its too chatoic
-        if person - (i+1) > 2:
-            print("Too chaotic")
-            return
-        
-        # if person not at initial position
-        if person != i + 1:
-            
-            # count the number of people with a number larger than them 
-            # ranging from one position in front of their original position
-            # to one person in front of their current position
-            for j in range(max(person - 2, 0), i):
-                if q[j] > person:
-                    bribes += 1
-            
-    print(bribes)
-    return 
-        
-    
 def main():
     """Main function
     """
 
-    q = [2, 1, 5, 3, 4]
-    print(minimumBribes(q))
-
-
+    S = ""
+    S_changes = [S]
+    
+    # read input
+    Q = int(input())
+    for _ in range(Q):
+        operation = input()
+        
+        # handle cases
+        if operation[0] == "1":
+            W = operation.split()[1]
+            S += W
+        elif operation[0] == "2":
+            to_delete = int(operation.split()[1])
+            S = S[:-to_delete]
+        elif operation[0] == "3":
+            index = int(operation.split()[1]) - 1
+            if index <= (len(S) - 1):
+                print(S[index])
+        else:
+            S = S_changes[-1]
+            S_changes.pop()
+            
+        print(S)
+                    
+        # store change
+        if operation[0] != "4" and S != S_changes[-1]:
+            S_changes.append(S)
+            
+    return S
+            
 # run file
 if __name__ == "__main__":
     main()
