da269d3881721811b9bbd671d9ce6e8625e13d37
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2022-12-10 01:30:20 +0000

    leetcode

--- Leetcode/leetcode.cpp
+++ Leetcode/leetcode.cpp
@@ -1,101 +1,59 @@
-// Title: Google Kick Start
-// Name: Christopher Robert Kitching
-// E-mail: christopher.kitching@manchester.ac.uk
-// Date created: 17/10/22
-// Last editied: 17/10/22
-// Description: File to test Leetcode solutions
-
-#include <vector>
-#include <iostream>
-#include <string>
+#include <limits>
 #include <algorithm>
-#include <unordered_set>
-<<<<<<< HEAD
+#include <iostream>
 
-using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    long long total{0};
+    long long result{0};
 
-int coinChange(vector<int>& coins, int n) {
-        
-    // initalise variables
-    int* dp = new int[n+1];
-    
-    dp[0] = 0; // 0 coins needed to make 0 total
-    
-    // sort array
-    sort(coins.begin(), coins.end());
-    
-    // determine how to make each possible value
-    // using the minimum number of coins
-    for(int i{1}; i<=n; i++){
-        
-        dp[i] = INT_MAX;
-        
-        for(auto c : coins){
-            
-            // if the value of the coin is greater than the value we are trying to make
-            // then move to making next value in the list
-            if(i - c < 0) break;
-            
-            // best way to make current amount
-            // is the min 
-            if(dp[i-c] != INT_MAX) dp[i] = min(dp[i], 1 + dp[i-c]);
+    long long dfs(TreeNode* root) {
+        if (root == nullptr) return 0;
 
-        }
-    }
-    
-    return dp[n] == INT_MAX ? -1 : dp[n];
-    
-}
-    
+        long long sum = root->val + dfs(root->left) + dfs(root->right);
+        result = std::max(result, sum * (total - sum));
 
-
-int main()
-{
-    vector<int> coins;
-    coins.push_back(1);
-    coins.push_back(2);
-    coins.push_back(5);
-    int n{11};
-
-    int change = coinChange(coins, n);
-    cout << change;
-=======
-#include <unordered_map>
-#include <set>
-
-using namespace std;
-
-int maxProfit(vector<int>& prices) {
-        
-    int N = prices.size();
-    int profit{0};
-    int buy_price;
-
-    for(int i{0}; i < N - 1; i++){
-
-        // find valley and buy
-        while(prices[i+1] <= prices[i] && i < N-1) i++;
-        buy_price = prices[i];
-
-        // find peak and sell
-        while(prices[i+1] > prices[i] && i < N-1) i++;
-        profit += prices[i] - buy_price;
-
+        return sum;
     }
 
-    return profit;
+    int maxProduct(TreeNode* root) {
+        total = dfs(root);
+        dfs(root);
+        return result % 1000000007;
+    }
+};
 
+// Function to build a small test tree
+TreeNode* buildTestTree() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(7);
 
+    return root;
 }
 
-int main()
-{
-    vector<int> v{1,2};
-    int x = maxProfit(v);
-    cout << x;
+int main() {
+    // Build a small test tree
+    TreeNode* root = buildTestTree();
 
+    // Use the maxProduct function to find the maximum product of the sum of two subtrees in the test tree
+    Solution solution;
+    int result = solution.maxProduct(root);
 
->>>>>>> 93889f7e6373b65265270926f4333466ee2ff2be
+    // Print the result
+    std::cout << "Maximum product of the sum of two subtrees: " << result << std::endl;
 
     return 0;
 }
