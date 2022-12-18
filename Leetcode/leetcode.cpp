c1f8ecbd258a56179e55001170b210394391f18c
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2022-12-18 01:11:02 +0000

    leetcode

--- Leetcode/leetcode.cpp
+++ Leetcode/leetcode.cpp
@@ -4,33 +4,43 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <queue>
 
 using namespace std;
 
+#define mp make_pair
 #define ll long long
 
-set<string> op = {"+", "-", "*", "/"};
+vector<int> dailyTemperatures(const vector<int>& temperatures) {
 
-ll evalRPN(vector<string>& tokens){
-    string t = tokens.back();
-    tokens.pop_back();
-    if(t != "+" && t != "-" && t != "*" && t != "/") return stoll(t);
-    else{
-        ll b = evalRPN(tokens);
-        ll a = evalRPN(tokens);
-        if(t == "+") return a+b;
-        else if(t == "-") return a-b;
-        else if(t == "*") return a*b;
-        else return a/b;
+    vector<int> ret(temperatures.size(), 0);
+    queue<pair<int, int>> q;
+    q.push(mp(0, temperatures[0]));
+    for(int i{1}; i < temperatures.size(); i++){
+        
+        if(q.empty()) q.push(mp(i, temperatures[i]));
+
+        while(!q.empty() && q.front().second < temperatures[i]){
+            ret[q.front().first] = i - q.front().first;
+            q.pop();
+        }
+
+        q.push(mp(i, temperatures[i]));
     }
+
+    while(!q.empty()){
+        ret[q.front().first] = 0;
+        q.pop();
+    }
+
+    return ret;
 }
 
 int main()
 {
-    vector<string> test = {"2","1","+","3","*"};
-    ll res;
-    res = evalRPN(test);
-    cout << res << endl;
+    vector<int> test = {73,74,75,71,69,72,76,73};
+    vector<int> res;
+    res = dailyTemperatures(test);
 
     return 0;
 }
