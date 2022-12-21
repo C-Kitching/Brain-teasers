82bf627d7f581098ea97599b56a54a8f560851fc
C-Kitching <113296322+C-Kitching@users.noreply.github.com>
2022-12-21 15:55:32 +0000

    leetcode

--- Leetcode/leetcode.cpp
+++ Leetcode/leetcode.cpp
@@ -5,42 +5,72 @@
 #include <string>
 #include <set>
 #include <queue>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 #define mp make_pair
+#define WHITE 0
+#define RED 1
+#define BLUE 2
 #define ll long long
 
-vector<int> dailyTemperatures(const vector<int>& temperatures) {
-
-    vector<int> ret(temperatures.size(), 0);
-    queue<pair<int, int>> q;
-    q.push(mp(0, temperatures[0]));
-    for(int i{1}; i < temperatures.size(); i++){
+bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         
-        if(q.empty()) q.push(mp(i, temperatures[i]));
+    // create adjacency list and colour map
+    unordered_map<int, unordered_set<int>> A;
+    unordered_map<int, int> colours; 
+    for(auto& edge : dislikes){
+        A[edge[0]].insert(edge[1]);
+        A[edge[1]].insert(edge[0]);
+        if(colours.find(edge[0]) == colours.end()) colours[edge[0]] = WHITE;
+        if(colours.find(edge[1]) == colours.end()) colours[edge[1]] = WHITE;
+    }
 
-        while(!q.empty() && q.front().second < temperatures[i]){
-            ret[q.front().first] = i - q.front().first;
+    // start bfs from each node
+    for(int i{1}; i <= n; i++){
+        
+        // push initial node to queue
+        queue<int> q;
+        q.push(i);
+
+        // bsf
+        while(!q.empty()){
+
+            // get next node
+            int node = q.front();
             q.pop();
+
+            // first time at node
+            if(colours[i] == WHITE) colours[i] = RED;
+
+            // set all neighbours to opposite colour
+            for(auto& neighbour : A[i]){
+
+                // found an invalid connection
+                if(colours[i] == colours[neighbour]) return false;
+
+                // else set colour of neighbour opposite to current node
+                if(colours[neighbour] == WHITE){
+                    colours[i] == RED ? colours[neighbour] = BLUE : colours[neighbour] = RED;
+
+                    // add neighbour to queue
+                    q.push(neighbour);
+                }
+            }
         }
-
-        q.push(mp(i, temperatures[i]));
     }
 
-    while(!q.empty()){
-        ret[q.front().first] = 0;
-        q.pop();
-    }
-
-    return ret;
+    return true;
 }
 
 int main()
 {
-    vector<int> test = {73,74,75,71,69,72,76,73};
-    vector<int> res;
-    res = dailyTemperatures(test);
+    vector<vector<int>> test = {{1,2},{1,3},{2,4}};
+    bool b = possibleBipartition(4, test);
+
+    cout << b << endl;
 
     return 0;
 }
