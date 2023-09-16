899bba235b812a7d4127d020745f1377d75a16ad
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2023-09-16 20:11:19 +0100

    optiver

--- Leetcode/leetcode.py
+++ Leetcode/leetcode.py
@@ -1,46 +1,70 @@
-def find_lcm(num1, num2):
-    if(num1>num2):
-        num = num1
-        den = num2
-    else:
-        num = num2
-        den = num1
-    rem = num % den
-    while(rem != 0):
-        num = den
-        den = rem
-        rem = num % den
-    gcd = den
-    lcm = int(int(num1 * num2)/int(gcd))
-    return lcm
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
-def subarrayLCM(nums, k):
+
+#
+# Complete the 'game_value' function below.
+#
+# The function is expected to return a DOUBLE.
+# The function accepts INTEGER deck_size as parameter.
+# deck_size is an even non-negative integer
+#
+def optimal_value(N):
+    """
+    Calculate optimal payoff of N size deck using dynamic programming
+    """
+    
+    # dynamic programming memory
+    memo = {}
+    
+    def dp(red, green):
+        """
+        Recursive function to calculate the expected value
+        """
         
-    count = 0
-    for i in range(len(nums)):
-        for j in range(i, len(nums)):
-            l = nums[i:j+1]
-            if len(l) > 1:
-                num1 = l[0]
-                num2 = l[1]
-                lcm = find_lcm(num1, num2)
+        if red == 0:
+            return 0
+        elif green == 0:
+            return red
+        elif (red, green) in memo:
+            return memo[(red, green)]
+            
+        # calculate the expected value if you draw a card
+        # i.e prob of drawing that card plus contribution to payoff, then you have to keep playing with less cards
+        draw_red = red/(red+green) * (1 + dp(red - 1, green)) # red
+        draw_green = green/(red+green) * (-1 + dp(red, green - 1)) # green
+            
+        # expected payoff if we stopped here
+        stop = 0
+        
+        # choose the maximum of drawing a card or stopping
+        memo[(red, green)] = max(draw_red + draw_green, stop)
+        return memo[(red, green)]
+        
+    return dp(N//2, N//2)
+    
 
-                for p in range(2, len(l)):
-                    lcm = find_lcm(lcm, l[p])
+def game_value(deck_size):
+    # Write your code here
+    
+    # call helper function
+    print(optimal_value(deck_size))
+    
 
-                if lcm == k: count += 1
-            else:
-                lcm = l[0]
-                if lcm == k: count += 1
-                
-    return count
+    
+    
+    
+        
+    
+    
+    
 
-def main():
-
-    x = [3,6,2,7,1]
-    k = 6
-    count = subarrayLCM(x, k)
-    print(count)
-
-main()
+if __name__ == '__main__':
+    deck_size = 8
+    game_value(deck_size)
