3ab1c07f1d1be9e335db513ecd3f1a4676b2c5c8
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2023-09-19 17:45:09 +0100

    leetcode

--- Leetcode/leetcode.cpp
+++ Leetcode/leetcode.cpp
@@ -12,36 +12,41 @@ using namespace std;
 
 #define mp make_pair
 
-bool wordBreak(string s, unordered_set<string> &dict) {
-    if(dict.size()==0) return false;
-    
-    vector<bool> dp(s.size()+1,false);
-    dp[0]=true;
-    
-    for(int i=1;i<=s.size();i++)
-    {
-        for(int j=i-1;j>=0;j--)
-        {
-            if(dp[j])
-            {
-                string word = s.substr(j,i-j);
-                if(dict.find(word)!= dict.end())
-                {
-                    dp[i]=true;
-                    break; //next i
-                }
-            }
-        }
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        
+    int m = matrix.size();
+    int n = matrix[0].size();
+    vector<int> res(m*n);
+    int i{0};
+    int l{0}, r{n-1}, u{0}, d{m-1};
+
+    while(l <= r && u <= d){
+
+        // add top layer
+        for(int col{l}; col <= r; col++) res[i++] = matrix[u][col];
+        if(++u > d) break;
+
+        // add right layer
+        for(int row{u}; row <= d; row++) res[i++] = matrix[row][r];
+        if(--r < l) break;
+
+        // add bottom layer
+        for(int col{r}; col >= l; col--) res[i++] = matrix[d][col];
+        if(--d < u) break;
+
+        // add left layer
+        for(int row{d}; row >= u; row--) res[i++] = matrix[row][l];
+        if(++l > r) break;
     }
-    
-    return dp[s.size()];
+
+    return res;
+
 }
 
 int main()
 {
-    string haystack = "leetcode";
-    unordered_set<string> set = {"leet", "code"};
-    cout << wordBreak(haystack, set); 
+    vector<vector<int>> v = {{1,2,3},{4,5,6},{7,8,9}};
+    spiralOrder(v);
 
     return 0;
 }
