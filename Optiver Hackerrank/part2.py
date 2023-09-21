d9bb2846f325dd94db6ebeb8ce17ad54ea805ce0
Christopher Kitching <113296322+MacSwish@users.noreply.github.com>
2023-09-21 10:05:54 +0100

    leetcode

--- Optiver Hackerrank/part2.py
+++ Optiver Hackerrank/part2.py
@@ -1,37 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'game_value' function below.
+#
+# The function is expected to return a DOUBLE.
+# The function accepts INTEGER deck_size as parameter.
+# deck_size is an even non-negative integer
+#
 def optimal_value(N):
-    if N % 2 != 0:
-        raise ValueError("Deck size must be even")
-
-    half_N = N // 2
-
-    # Initialize a memoization table to store computed values
+    """
+    Calculate optimal payoff of N size deck using dynamic programming
+    """
+    
+    # dynamic programming memory
     memo = {}
-
-    # Helper function to calculate the expected value
+    
     def dp(red, green):
+        """
+        Recursive function to calculate the expected value
+        """
+        
         if red == 0:
-            return 0  # No red cards left, so stop
+            return 0
         elif green == 0:
             return red
         elif (red, green) in memo:
             return memo[(red, green)]
-
-        # Calculate the expected value if you draw a red card
-        draw_red = (red / (red + green)) * (1 + dp(red - 1, green))
-
-        # Calculate the expected value if you draw a green card
-        draw_green = (green / (red + green)) * (-1 + dp(red, green - 1))
-
-        # Calculate the expected value if you stop drawing cards
+            
+        # calculate the expected value if you draw a card
+        # i.e prob of drawing that card plus contribution to payoff, then you have to keep playing with less cards
+        draw_red = red/(red+green) * (1 + dp(red - 1, green)) # red
+        draw_green = green/(red+green) * (-1 + dp(red, green - 1)) # green
+            
+        # expected payoff if we stopped here
         stop = 0
-
-        # Choose the maximum expected value
-        memo[(red, green)] = max(draw_red, draw_green, stop)
+        
+        # choose the maximum of drawing a card or stopping
+        memo[(red, green)] = max(draw_red + draw_green, stop)
         return memo[(red, green)]
+        
+    return dp(N//2, N//2)
+    
 
-    return dp(half_N, half_N)
+def game_value(deck_size):
+    # Write your code here
+    
+    # call helper function
+    print(optimal_value(deck_size))
 
-# Example usage:
-N = 52  # Deck size must be even
-value = optimal_value(N)
-print(f"The optimal value for a deck of size {N} is ${value:.2f}")
+if __name__ == '__main__':
+    deck_size = 52
+    game_value(deck_size)
